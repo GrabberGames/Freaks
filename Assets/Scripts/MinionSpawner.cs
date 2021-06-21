1ae994ae75f5d2a7a4aed6bcfa091be6f4600009
wlvlrtm <kimsk200012@gmail.com>
2021-06-21 22:41:22 +0900

    소환 지점 구현
    1. 프릭스 소환 지점을 다리 3곳으로 수정
    
    2. 무작위로 프릭스 소환 지점 택1 및 프릭스 소환 구현
    
    3. 2분 간격으로 다음 웨이브 시작 구현 (샘플: 1 ~ 10 웨이브)
    
    4. 원거리/근거리 프릭스 구분 적용
    
    ///
    
    - 기타 사항
    웨이브 시작을 알릴 수 있는 2분 간격 타이머 UI를 하나 구현해보려고 합니다.
    디버깅이나 이후 UI 작업 시에도 활용할 수 있을 것 같습니다.
    그런데 코루틴에서 사용되는 2분 타이머를 어떻게 외부(UI) 구역으로 빼낼 수 있을 지 방법을 모르겠네요.
    도움 부탁 드립니다.

--- Assets/Scripts/MinionSpawner.cs
+++ Assets/Scripts/MinionSpawner.cs
@@ -2,11 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Wave
+{
+    w0,
+    w1, w2, w3, w4, w5,
+    w6, w7, w8, w9, w10
+}
+
+public enum SpawnPoint
+{
+    SpawnPoint_Black_N, SpawnPoint_Black_S, SpawnPoint_Black_E
+}
+
+
 public class MinionSpawner : MonoBehaviour
 {
-    public GameObject Minion;
+    public GameObject[] Minion;     // Minion: close range || Minion_2: long range
+    public int spawnRate = 1;
+    public int waveTimer = 2;
+    public int cm_num = 3;
+    public int lm_num = 2;
 
-    public int spawnRate;
+    private SpawnPoint spawnPoint = 0;
+    private Wave wave = Wave.w0;
+    private int wave_num = 0;
+    private int randomSelect;
+
 
     // Start is called before the first frame update
     void Start()
@@ -14,20 +35,48 @@ public class MinionSpawner : MonoBehaviour
         StartCoroutine(MinionSpawn());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
 
     IEnumerator MinionSpawn()
     {
         while(true)
         {
-            yield return new WaitForSeconds(spawnRate);
-            Instantiate(Minion, this.transform.position, Quaternion.identity);
-            Debug.Log("Spawned");
+            randomSelect = Random.Range(0, 3);  // enum SpawnPoint (0, 1, 2)
+            spawnPoint += randomSelect;
+
+
+            yield return new WaitForSeconds(waveTimer * 60);    // sec -> mins
+
+
+            if (gameObject.name == spawnPoint.ToString())   // if, random selected point
+            {
+                if (wave == Wave.w0 && wave_num < System.Enum.GetValues(typeof(Wave)).Length - 1)   // Wave.w0 == break time || 1 <= wave_num <= 10; Wave.Length == 11
+                {
+                    wave_num += 1;
+                    wave += wave_num;
+                    Debug.Log("wave: " + wave);
+
+                    while (cm_num > 0)
+                    {
+                        Instantiate(Minion[0], this.transform.position, Quaternion.identity);   // close range minion spawn
+                        yield return new WaitForSeconds(spawnRate);     // 1sec wait
+                        Debug.Log(gameObject.name + " Spawned");
+                        cm_num--;
+                    }
+
+                    while (lm_num > 0)
+                    {
+                        Instantiate(Minion[1], this.transform.position, Quaternion.identity);   // long range minion spawn
+                        yield return new WaitForSeconds(spawnRate);     // 1sec wait
+                        Debug.Log(gameObject.name + " Spawned");
+                        lm_num--;
+                    }
+                    wave = Wave.w0;     // break time activate (2 min)
+                }
+            }
+            else
+            {
+                break;
+            }
         }
     }
 }
