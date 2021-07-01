157b032de9c512b3a84ebecb21ca0b3911184a3f
PooHappy <av3rs1on@naver.com>
2021-07-01 16:54:48 +0900

    영웅의 기본 시스템 구현
    1. 캐릭터 에셋 임포트
    2. Layer - Tempcast, Walkable
    3. Road - BSP Tree, Super Collision Type
    4. 현재 캐릭터 이동은 방향키로 구현.
    5. 캐릭터 스텟 임시 구현

---
+++ Assets/Scripts/Charactor_Stat.cs
@@ -0,0 +1,9 @@
+public class Charactor_Stat
+{
+    protected int Hp;
+    protected int Mp;
+    protected int MoveSpeed;
+    protected int AttackSpeed;
+    protected int Range;
+    protected int Ammor;
+}
