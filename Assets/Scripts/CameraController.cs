a2feb183092276924e0076f4098d31f67322c96c
PooHappy <av3rs1on@naver.com>
2021-07-12 23:06:50 +0900

    카메라 스크롤 구현
    카메라 이동은 추후 스케쥴에따라 구현하겠습니다

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float ZoomSpeed= 10.0f;
+    public float Distance = 10.0f;
+
+    private Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Zoom();
+    }
+    void Zoom()
+    {
+        Distance += Input.GetAxis("Mouse ScrollWheel") * -1 * ZoomSpeed;
+        Distance = Mathf.Clamp(Distance, 30f, 121f);
+        mainCamera.fieldOfView = Distance;
+    }
+}
