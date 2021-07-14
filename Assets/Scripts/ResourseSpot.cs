92df13647ee86e02644ca16ee92117422b074f1a
PooHappy <av3rs1on@naver.com>
2021-07-14 18:02:32 +0900

    자원지 위에 워크샵
    1. 현재 자원량에 따라 색 변경
    2. 화살표 마다 자원량 입력완료

---
+++ Assets/Scripts/ResourseSpot.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourseSpot : MonoBehaviour
+{
+    public int RemainResource;
+    [SerializeField]
+    private int GetResourcePerOnce = 50;
+    private Material navy, blue, sky_blue;
+    private Material material;
+
+    void Start()
+    {
+        material = this.gameObject.GetComponent<Renderer>().material;
+        navy = GetComponentInParent<ResourceManager>().materials[0];
+        blue = GetComponentInParent<ResourceManager>().materials[1];
+        sky_blue = GetComponentInParent<ResourceManager>().materials[2];
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        SetColor();
+    }
+    void SetColor()
+    {
+        if(RemainResource >= 801)
+        {
+            gameObject.GetComponent<Renderer>().material = navy;
+        }
+        else if(RemainResource >= 401)
+        {
+            gameObject.GetComponent<Renderer>().material = blue;
+        }
+        else if(RemainResource >= 1)
+        {
+            gameObject.GetComponent<Renderer>().material = sky_blue;
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
