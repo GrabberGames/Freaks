83574686c5ae097703d416e31ec99bbb3324f729
wlvlrtm <kimsk200012@gmail.com>
2021-08-16 21:51:33 +0900

    카메라 위치 점프 기능 구현 중

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private Camera mainCamera;
+    public Vector3 offset;
     public float zoomSpeed = 10.0f;
 
+
+    private Camera mainCamera;
     private float[] rangeY = { 142f, 348.25f };
     private float[] initialrangeY = { 142f, 348.25f };
     private float[] rangeZ = { 9.2f, 64.2f };
     private float[] initialrangeZ = { 9.2f, 64.2f };
     private float currentY;
     private float currentZ;
-
     private float delta;
     private float nor;
     private Transform player; // Player object
@@ -21,12 +22,14 @@ public class CameraController : MonoBehaviour
     private Transform alter; // Alter object
 
     // private Vector3 lookOffset;    // The position of camera which view target
+
+
     private Vector3 initialPosition;
     private Quaternion initialRotation;
 
     void Awake() {
        initialPosition = transform.position;
-       initialRotation = transform.rotation ;
+       initialRotation = transform.rotation;
     }
 
 
@@ -49,20 +52,24 @@ public class CameraController : MonoBehaviour
         currentY = transform.position.y;
         currentZ = transform.position.z;
     }
+
+
     void Zoom()
     {
+        
         if ((delta = Input.GetAxis("Mouse ScrollWheel") * -5 * zoomSpeed) != 0)
         {
             currentY = Mathf.Clamp(currentY + delta * nor, rangeY[0], rangeY[1]);
             currentZ = Mathf.Clamp(currentZ + delta, rangeZ[0], rangeZ[1]);
         }
         mainCamera.transform.position = new Vector3(mainCamera.transform.position.x, currentY, currentZ);
+        
     }
 
     private void Move()
     {
-
-        if(Input.mousePosition.x > Screen.width){
+        
+        if (Input.mousePosition.x > Screen.width){
             transform.Translate(2.0f, 0, 0);
         } 
        
@@ -82,11 +89,11 @@ public class CameraController : MonoBehaviour
             rangeZ[0] += 4; rangeZ[1] += 4;
             transform.position = new Vector3(transform.position.x, transform.position.y , currentZ);
         }
-
+        
         if (Input.GetKeyDown("space"))
         {
-            transform.rotation = Quaternion.Euler(new Vector3(75.0f, -180.0f, transform.rotation.z));
-            transform.position = new Vector3(player.position.x, transform.position.y, currentZ - rangeZ[0]);
+            //transform.rotation = Quaternion.Euler(new Vector3(75.0f, -180.0f, 0));
+            transform.position = player.position + offset;
         }
 
         if (Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt))
