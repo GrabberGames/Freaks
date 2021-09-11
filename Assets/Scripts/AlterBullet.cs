6b6b95f0500f3bbb6f8c89a5cfbc751b03d8fab9
PooHappy <av3rs1on@naver.com>
2021-09-11 15:27:23 +0900

    알터공격
    알터공격

--- Assets/Scripts/AlterBullet.cs
+++ Assets/Scripts/AlterBullet.cs
@@ -13,7 +13,7 @@ public class AlterBullet : MonoBehaviour
     public GameObject enemy;
 
     private Vector3 enemyPos;
-    private float BulletSpeed = 8f;
+    private float BulletSpeed = 1f;
     
     private int State = 0;
     // 1 = 투사체 날라가는 중
@@ -27,13 +27,9 @@ public class AlterBullet : MonoBehaviour
     {
         if (other.transform.CompareTag("BlackFreaks"))
         {
-            FX_Alter_Projectile.Play(false);
-            FX_Alter_Hit = Instantiate(FX_Alter_Hit_Pre);
-            FX_Alter_Hit.transform.SetParent(this.gameObject.transform);
-            FX_Alter_Hit.transform.position = gameObject.transform.position;
-            FX_Alter_Hit.transform.rotation = Quaternion.LookRotation(enemyPos - FX_Alter_Hit.transform.position);
-            FX_Alter_Hit.Play(true);
             State = 2;
+            FX_Alter_Projectile_Pre.Play(false);
+            FX_Alter_Hit_Pre.Play(true);
             StartCoroutine(DeleteThis());
         }
     }
@@ -41,31 +37,13 @@ public class AlterBullet : MonoBehaviour
     void Update()
     {
         enemyPos = new Vector3(enemy.transform.position.x, enemy.transform.position.y , enemy.transform.position.z);
-        switch(State)
-        {
-            case 1: //투사체 날라가는 중
-                FX_Alter_Projectile.transform.position = transform.position;
-                FX_Alter_Projectile.transform.rotation = Quaternion.identity;
-                transform.position -= (transform.position - enemyPos) * BulletSpeed * Time.deltaTime;
-                transform.rotation = Quaternion.LookRotation(enemyPos - transform.position);
-                break;
-            case 2: // 투사체 피격
-                FX_Alter_Hit.transform.position = transform.position;
-                break;
-        }
+        transform.position -= (transform.position - enemyPos) * BulletSpeed * Time.deltaTime;
+        transform.rotation = Quaternion.LookRotation(enemyPos - transform.position);
     }
     void StartProjectile()
     {
-        FX_Alter_Projectile = Instantiate(FX_Alter_Projectile_Pre);
-        FX_Alter_Projectile.transform.SetParent(this.gameObject.transform);
-        FX_Alter_Projectile.transform.position = transform.position;
-        FX_Alter_Projectile.transform.rotation = Quaternion.identity;
-        FX_Alter_Projectile.Play(true);
-        for(int i = 0; i < 3; i++)
-        {
-            FX_Alter_Projectile.transform.GetChild(i).GetComponent<ParticleSystem>().Play(true);
-        }
         State = 1;
+        FX_Alter_Projectile_Pre.Play(true);
     }
     IEnumerator DeleteThis()
     {
