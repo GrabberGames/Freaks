bc513baa375fe38f3b5b0057b4d4388ca095d03b
PooHappy <av3rs1on@naver.com>
2021-09-12 16:36:54 +0900

    PooHappy / '배수용 - 2021/09/12) 알터 공격 이펙트 최종 수정 완료

---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public GameObject enemy;
+    private Vector3 enemyPos;
+    private ParticleSystem particleSystem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void InitSetting(GameObject enemy)
+    {
+        this.enemy = enemy;
+        particleSystem = GetComponent<ParticleSystem>();
+        particleSystem.Play(true);
+        StartCoroutine(Delete());
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(enemy != null)
+        {
+            enemyPos = new Vector3(enemy.transform.position.x, enemy.transform.position.y, enemy.transform.position.z);
+            transform.position -= (transform.position - enemyPos);
+            transform.rotation = Quaternion.LookRotation(enemyPos - transform.position);
+        }
+    }
+    IEnumerator Delete()
+    {
+        yield return new WaitForSeconds(particleSystem.main.startLifetimeMultiplier);
+        Destroy(gameObject);
+    }
+}
