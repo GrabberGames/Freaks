64cc5c898a0dd50ca29fc118ee803b0812c389e7
Wise-eun <hee1871@naver.com>
2022-03-21 22:56:09 +0900

    자원지 위치,색 수정 및 워크샵 색 수정 & 블랙타워,워크샵 페이드아웃구현

--- Assets/Scripts/TowerAttack.cs
+++ Assets/Scripts/TowerAttack.cs
@@ -9,6 +9,8 @@ public class TowerAttack : Stat
     public GameObject player;
     public ParticleSystem fx_blackTower;
     public ParticleSystem fx_hit;
+
+ 
     Stat _stat;
 
     private float AttackPerSeconds = 4f;
@@ -22,6 +24,8 @@ public class TowerAttack : Stat
         base.Init();
 
         bulletSpawnPosition = new Vector3(transform.position.x, transform.position.y + 18.98f, transform.position.z - 0.29f);
+
+       // StartCoroutine("FadeOut"); //fadeout할때 쓸 코드
     }
     public override void DeadSignal()
     {
@@ -34,6 +38,7 @@ public class TowerAttack : Stat
     void Start()
     {
         Init();
+   
     }
 
     private void Update()
@@ -47,6 +52,7 @@ public class TowerAttack : Stat
         }
         if((player.transform.position - transform.position).magnitude <=  ATTACK_RANGE)
         {
+            
             if (isAttack)
                 return;
             else
@@ -70,4 +76,50 @@ public class TowerAttack : Stat
         yield return new WaitForSeconds(AttackPerSeconds - fx_blackTower.main.startDelayMultiplier);
         isAttack = false;
     }
+
+    IEnumerator FadeOut()
+    {
+        Debug.Log("2초후에 fadeout ");
+        yield return new WaitForSeconds(2f);
+        Debug.Log(" fadeout !! ");
+
+        MeshRenderer mr_c1 = transform.GetChild(0).gameObject.GetComponent<MeshRenderer>();
+        mr_c1.material.shader = Shader.Find("UI/Unlit/Transparent");
+
+        MeshRenderer mr_c2 = transform.GetChild(1).gameObject.GetComponent<MeshRenderer>();
+        mr_c2.material.shader = Shader.Find("UI/Unlit/Transparent");
+
+        for (int i = 30; i >= 0; i--)
+            {
+         
+
+            float f = i /30.0f ;
+        
+                float c1_r = transform.GetChild(0).gameObject.GetComponent<MeshRenderer>().material.color.r;
+                float c1_g = transform.GetChild(0).gameObject.GetComponent<MeshRenderer>().material.color.g;
+                float c1_b = transform.GetChild(0).gameObject.GetComponent<MeshRenderer>().material.color.b;
+                float c1_a = transform.GetChild(0).gameObject.GetComponent<MeshRenderer>().material.color.a;
+            Color c1 = new Color(c1_r , c1_g , c1_b );
+            c1.a = f;
+     
+
+                float c2_r = transform.GetChild(1).gameObject.GetComponent<MeshRenderer>().material.color.r;
+                float c2_g = transform.GetChild(1).gameObject.GetComponent<MeshRenderer>().material.color.g;
+                float c2_b = transform.GetChild(1).gameObject.GetComponent<MeshRenderer>().material.color.b;
+                float c2_a = transform.GetChild(1).gameObject.GetComponent<MeshRenderer>().material.color.a;
+            Color c2 = new Color(c2_r , c2_g , c2_b );
+            c2.a = f ;
+           
+
+            
+            transform.GetChild(0).gameObject.GetComponent<MeshRenderer>().material.color = c1;
+            transform.GetChild(1).gameObject.GetComponent<MeshRenderer>().material.color = c2;
+            
+            yield return new WaitForSeconds(0.04f);
+            }
+        Destroy(transform.GetChild(0).gameObject.transform.GetChild(0).gameObject); //파티클 시스템 제거
+        Destroy(transform.GetChild(2).gameObject);
+    }
+    
+
 }
