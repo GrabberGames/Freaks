6c52c5f5a8d18d8867d71e830c75c7f2760e75e1
PooHappy <av3rs1on@naver.com>
2022-08-04 08:46:09 +0900

    PooHappy / '배수용 - 2022/08/04) 스케쥴
    1. 볼륨 Text

--- Assets/Scripts/SetVolume.cs
+++ Assets/Scripts/SetVolume.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Audio;
 
 public class SetVolume : MonoBehaviour
 {
+    public Text text;
+
     public AudioMixer mixer;
     
 
     public void SetLevel(float sliderVal)
     {
         mixer.SetFloat("masterVolume", Mathf.Log10(sliderVal)*20);
+        text.text = ((int)(sliderVal * 100)).ToString();
     }
 }
